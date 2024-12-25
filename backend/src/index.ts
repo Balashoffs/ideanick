9cb9c088b3e8691d9a1cda2f2a9dbf5bb822f46c
balashoffs <balashov87aleksandr@gmail.com>
2024-12-25 12:05:02 +0300

    add express module on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello!')
+import express from 'express'
+
+const expressApp = express()
+expressApp.get('/ping', (req, res) => {
+  res.send('Pong!')
+})
+
+expressApp.listen(3000, () => {
+  console.info('Express server listening on port ' + process.env.PORT)
+})
