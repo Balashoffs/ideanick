b2b609bfc4352343d0fc96b7a1b2fe1d5fc058cf
bau <bau@liis.su>
2024-12-27 10:59:56 +0300

    add layout react route

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,14 +3,17 @@ import { TrpcProvider } from './lib/trpc.tsx'
 import { AllIdeasPage } from './pages/AllIdeasPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
 import { getViewIdeaRoute, getAllIdeasRoute, viewIdeaRouteParams } from './lib/routes'
+import { Layout } from './components/Layout'
 
 export const App = () => {
   return (
     <TrpcProvider>
       <BrowserRouter>
         <Routes>
-          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-          <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+          <Route element={<Layout/>}>
+            <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
 
