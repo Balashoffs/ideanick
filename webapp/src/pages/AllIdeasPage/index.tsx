6589dd4c7dd0f74ec19cf18842de03ff5b8c4e76
bau <bau@liis.su>
2024-12-27 10:43:31 +0300

    meet with loadsh and zod packages

--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -1,6 +1,6 @@
 import { trpc } from '../../lib/trpc'
 import { Link } from 'react-router-dom'
-import { getViewIdeaRoute, viewIdeaRouteParams } from '../../lib/routes.ts'
+import { getViewIdeaRoute, } from '../../lib/routes.ts'
 
 export const AllIdeasPage = () => {
   const { data, error, isLoading, isError } = trpc.getIdeas.useQuery()
@@ -17,7 +17,7 @@ export const AllIdeasPage = () => {
         {data.map((idea) => {
           return (
             <div key={idea.nick}>
-              <h2><Link to={getViewIdeaRoute(viewIdeaRouteParams )}>{idea.name}</Link></h2>
+              <h2><Link to={getViewIdeaRoute({ideaNick: idea.nick})}>{idea.name}</Link></h2>
               <p> {idea.description}</p>
             </div>
           )
--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,17 +1,29 @@
 import { useParams } from 'react-router-dom'
-import { type ViewIdeaRouteParams } from '../../lib/routes'
+import { ViewIdeaRouteParams } from '../../lib/routes.ts'
+import { trpc } from '../../lib/trpc.tsx'
+// import { type ViewIdeaRouteParams } from '../../lib/routes'
 
-export const  ViewIdeaPage = () => {
-  const {ideaNick} = useParams() as ViewIdeaRouteParams
+export const ViewIdeaPage = () => {
+  const { ideaNick } = useParams() as ViewIdeaRouteParams
+
+  const { data, error, isLoading, isError } = trpc.getIdea.useQuery({ ideaNick })
+  if (isLoading) {
+    return <span>Loading ...</span>
+  }
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+  if (!data.idea) {
+    return <span>Idea not found</span>
+  }
+
   return (
     <div>
-      <h1>{ideaNick}</h1>
-      <p>Description of idea 1...</p>
-      <div>
-        <p>Text paragraph 1 of idea 1 ...</p>
-        <p>Text paragraph 2 of idea 1 ...</p>
-        <p>Text paragraph 3 of idea 1 ...</p>
-      </div>
+      <h1>{data.idea.name}</h1>
+      <p>{data.idea.description}</p>
+      <div
+        dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </div>
   )
 }
